cffecc122c4a2ae9335aadef9a8caa8c7668a6af
David <dvdalilue@gmail.com>
2014-06-23 00:31:55 -0430

    Se documento hasta lo que se ha definido. Solo falta el metodo 'solve' de LCR.

--- bfs.rb
+++ bfs.rb
@@ -1,4 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Juego de Rock, Paper & Sccisors
+#
+# Author::    David Lilue  09-10444. Luis Miranda 10-10463
+# Copyright:: Copyright (c) 2014 Universidad Simon Bolivar
+# License::   Distribuye bajo los mismos terminos de Ruby
+
+# Modulo para el recorrido en BFS(level - order).
+# El metodo bfs solo recorre, devolviendo nodo a nodo.
+# El metodo find, busca el elemento que cumpla el predicado.
+# El metodo path, devuelve el camino al elemento que cumpla el predicado.
+# El metodo walk, realiza una accion en todos los nodos. Sino solo recorre.
 module BFS
+  # Recorre iterativamente desde start usando Breadth-First Search.
   def bfs(start)
     q = [start]
     visitado = []
@@ -12,7 +26,8 @@ module BFS
       end
     end
   end
-  
+  # Busca el primer elemento de la estructura que cumpla el
+  # predicado(predicate) haciendo uso del metodo bfs.
   def find(start, predicate)
     if start.respond_to? 'bfs'
       start.bfs(start) { |nodo| return nodo if predicate.call(nodo.value) }
@@ -20,7 +35,8 @@ module BFS
       puts "*** find: \'#{start}\' no es una estructura que pueda ser recorrida en BFS"
     end
   end
-
+  # Devuelve el camino desde start hasta el nodo que cumpla el
+  # predicado(predicate) haciendo uso del metodo bfs.
   def path(start, predicate)
     if start.respond_to? 'bfs'
       p = { start => [] }
@@ -34,7 +50,9 @@ module BFS
       puts "*** path: \'#{start}\' no es una estructura que pueda ser recorrida en BFS"
     end    
   end
-
+  # Recorre toda la estructura desde start, aplicando la
+  # accion(action), a todos los nodos vistados y los devuelve
+  # en una lista.
   def walk(start, action=lambda {|doe|})              
     if start.respond_to? 'bfs'
       visitados = []
@@ -49,39 +67,47 @@ module BFS
   end
 end
 
+# Arbol binario, con un solo valor en los nodos.
 class BinTree
   include BFS
-  attr_accessor :value, # Valor almacenado en el nodo
-                :left,  # BinTree izquierdo
-                :right  # BinTree derecho
-  
+  # Valor almacenado en el nodo.
+  attr_accessor :value
+  # BinTree izquierdo.
+  attr_accessor :left
+  # BinTree derecho.
+  attr_accessor :right
+  # Inicializa los atributos del arbol con los parametros.
   def initialize(v,l,r)
     @value = v
     @left  = l
     @right = r
   end
-
+  # Itera sobre los hijos de nodo.
   def each b
     b.call(@left)  unless @left.nil?
     b.call(@right) unless @right.nil?
   end
-
+  # Devuelve una version imprimible(entendible) del nodo.
   def to_s
     "#{@value}"
     #"(#{@value}-#{@left.to_s}-#{@right.to_s})"
   end
 end
 
+# Grafo de nodos donde cada nodo puede tener \'n\' hijos.
 class GraphNode
   include BFS
-  attr_accessor :value,   #Valor almacenado en el nodo
-                :children #Arreglo de sucesores GraphNode
+  #Valor almacenado en el nodo.
+  attr_accessor :value
+  #Arreglo de sucesores GraphNode.
+  attr_accessor :children
 
+  # Inicializa los atributos del arbol con los parametros.
   def initialize(v,c)
     @value    = v
     @children = c
   end
-
+  # Itera sobre los hijos del nodo.
   def each b
     @children.each do |c|
       b.call(c)
@@ -89,17 +115,29 @@ class GraphNode
   end
 end
 
+# Modela el estado de un problema de busqueda sobre un árbol
+# implícito de expansión. 
 class LCR
+  # Hash que mantiene la información del estado.
   attr_reader :value
-  
+  # Inicializa value con un nuevo Hash con la llaves
+  # where, left, right. Cada una con un valor que es
+  # la información del estado.
   def initialize(where,left,right)
     
     left.map! { |c| c.to_sym }
     right.map! { |c| c.to_sym }
     
-    @value = {"where"=>where.to_sym,"left"=>left,"right"=>right}
+    @value = {
+      "where" => where.to_sym,
+      "left"  => left,
+      "right" => right
+    }
   end
-
+  # Resuelve el problema de búsqueda.
+  def solve
+  end
+  # Devuelve un string del Hash value.
   def to_s
     value.to_s
   end
